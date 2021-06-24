43561b59b88c344ac0ff899a537367a5803dbb01
Xinchao Song <git@xinchaosong.com>
2021-06-24 03:58:40 -0400

    Code cleanup

--- scripts/rdda/util.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-def convert_str_to_float_list(list_text):
-    valid_idx_0 = list_text.find('[') + 1
-    valid_idx_1 = list_text.find(']')
-    result_list = map(float, list_text[valid_idx_0:valid_idx_1].split(", "))
-
-    return result_list
